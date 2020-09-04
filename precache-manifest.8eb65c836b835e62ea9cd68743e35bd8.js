3b41669dbe112a0b78b0262be590d1d715ab5b7d
Symbol <Symbolistic@live.com>
2020-09-04 05:26:40 -0400

    Updates

---
+++ precache-manifest.8eb65c836b835e62ea9cd68743e35bd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feca59a7a167c38113c89c0337edb111",
+    "url": "/react-ts-sass-restaurant-app/index.html"
+  },
+  {
+    "revision": "b39e1852ef3ee4e186ee",
+    "url": "/react-ts-sass-restaurant-app/static/css/main.602cf340.chunk.css"
+  },
+  {
+    "revision": "0f3f54e173cac3cbb808",
+    "url": "/react-ts-sass-restaurant-app/static/js/2.5e8d9b6e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ts-sass-restaurant-app/static/js/2.5e8d9b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39e1852ef3ee4e186ee",
+    "url": "/react-ts-sass-restaurant-app/static/js/main.018ead0d.chunk.js"
+  },
+  {
+    "revision": "d0bf466878818f24551d",
+    "url": "/react-ts-sass-restaurant-app/static/js/runtime-main.c2310898.js"
+  },
+  {
+    "revision": "9d83f56b4e036ee104029316f8eec5cf",
+    "url": "/react-ts-sass-restaurant-app/static/media/cooking.9d83f56b.png"
+  },
+  {
+    "revision": "e46a9bdecce57d5672d904ca6b8f41a1",
+    "url": "/react-ts-sass-restaurant-app/static/media/cookingart.e46a9bde.jpg"
+  },
+  {
+    "revision": "ff3fea3de57447d62784718f98def3ff",
+    "url": "/react-ts-sass-restaurant-app/static/media/salad.ff3fea3d.jpg"
+  },
+  {
+    "revision": "47b6aae1a7c72c9aae45dd236e774f1e",
+    "url": "/react-ts-sass-restaurant-app/static/media/texture.47b6aae1.jpg"
+  },
+  {
+    "revision": "2011bc10a43026e429d9f59f44c1a513",
+    "url": "/react-ts-sass-restaurant-app/static/media/topimg.2011bc10.jpg"
+  }
+]);
